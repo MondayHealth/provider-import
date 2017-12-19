27b61a39fa3aa8adddb713e1f7ed899e3e2600ae
ixtli <chris.galardi@risingtidegames.com>
2017-12-19 09:58:49 -0500

    rename providers

--- alembic/models/providers.py
+++ alembic/models/providers.py
@@ -1,25 +1,139 @@
-from sqlalchemy import Column, String
+from sqlalchemy import String, Column, ForeignKey, Integer, Text, Boolean, \
+    DateTime, Table
+from sqlalchemy.orm import relationship
 
-from alembic.models.base import BaseBase
+from alembic.models.base import BaseBase, Base
+
+plan_provider_table = Table("plan_provider", Base.metadata,
+                            Column("plan_id", Integer, ForeignKey("plan.id")),
+                            Column("provider_id", Integer,
+                                   ForeignKey("provider.id")))
 
 
 class Provider(BaseBase):
     """
-      create_table "providers", force: :cascade do |t|
+      create_table "provider_records", force: :cascade do |t|
+        t.integer "payor_id"
+        t.string "accepted_plan_ids"
         t.string "first_name", null: false
         t.string "last_name", null: false
         t.string "license", null: false
+        t.string "address", null: false
+        t.string "phone"
+        t.text "specialties"
         t.datetime "created_at", null: false
         t.datetime "updated_at", null: false
-        t.index ["first_name", "last_name", "license"],
-            name: "index_providers_on_first_name_and_last_name_and_license",
-            unique: true
+        t.integer "provider_id"
+        t.integer "directory_id"
+        t.string "certificate_number"
+        t.boolean "certified"
+        t.integer "minimum_fee"
+        t.integer "maximum_fee"
+        t.boolean "sliding_scale"
+        t.boolean "free_consultation"
+        t.text "services"
+        t.text "languages"
+        t.text "treatment_orientations"
+        t.text "works_with_groups"
+        t.text "works_with_ages"
+        t.text "website_url"
+        t.text "accepted_payors"
+        t.string "license_number"
+        t.boolean "accepting_new_patients"
+        t.text "modalities"
+        t.integer "years_in_practice"
+        t.string "school"
+        t.integer "year_graduated"
+        t.string "license_state"
+        t.string "accepted_payment_methods"
+        t.datetime "source_updated_at"
+        t.index ["first_name", "last_name", "directory_id"],
+            name: "first_last_directory_id", unique: true
+        t.index ["first_name", "last_name", "payor_id"],
+            name: "first_last_payor_id", unique: true
       end
     """
+
+    payor = Column(Integer, ForeignKey("payor.id"))
+
+    plans_accepted = relationship("Plan", secondary=plan_provider_table,
+                                  back_populates="providers")
 
     first_name = Column(String(64), nullable=False)
 
     last_name = Column(String(64), nullable=False)
 
-    #
+    # Practice qualifcations or credentials
     license = Column(String(), nullable=False)
+
+    address = Column(Integer, ForeignKey("address.id"), nullable=False)
+
+    # TODO: List
+    specialties = Column(Text())
+
+    #
+    provider = Column(Integer, ForeignKey("provider.id"))
+
+    #
+    directory = Column(Integer, ForeignKey("directory.id"))
+
+    # Psychiatry board certificate number
+    certificate_number = Column(String())
+
+    # Whether or not they are certified on the ABPN database
+    certified = Column(Boolean())
+
+    minimum_fee = Column(Integer())
+
+    maximum_fee = Column(Integer())
+
+    sliding_scale = Column(Boolean())
+
+    free_consultation = Column(Boolean())
+
+    # TODO: List
+    # Subspecialties, pulled from goodtherapy
+    services = Column(Text())
+
+    # TODO: List
+    languages = Column(Text())
+
+    # TODO: List
+    # CBT, psychoanalytic, dialectic, etc.
+    treatment_orientations = Column(Text())
+
+    # TODO: List
+    # material conditions: comorbidity, identity, etc., faith
+    works_with_groups = Column(Text())
+
+    # TODO: List, ranges?
+    works_with_ages = Column(Text())
+
+    website_url = Column(String(2048))
+
+    # TODO: List
+    accepted_payors = Column(Text())
+
+    # State license database number
+    license_number = Column(String(64))
+
+    #
+    accepting_new_patients = Column(Boolean())
+
+    # TODO: List
+    # What sort of patients (couples, individuals, families, etc.)
+    modalities = Column(Text())
+
+    began_practice = Column(Integer())
+
+    school = Column(String(1024))
+
+    year_graduated = Column(Integer())
+
+    # TODO: Enum
+    license_state = Column(String())
+
+    # TODO: Enum
+    accepted_payment_methods = Column(String())
+
+    source_updated_at = Column(DateTime)
